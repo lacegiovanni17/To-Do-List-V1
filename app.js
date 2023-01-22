68bb6186619e8a2f1c9de946de1d51864fae0d47
Chidike Henry <dk1online25@gmail.com>
2023-01-22 23:55:58 +0100

    implementing EJS

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+//the app.use has to be done below the position where you called express in the file.
 app.use("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    let today = new Date();
-    let currentDay = today.getDay();
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Yay! It's the weekend!</h1>");
-        res.sendFile(__dirname + "/weekend.html")
-    } else {
-        res.sendFile(__dirname + "/weekday.html");
-    }
+  if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
+    res.render("List", { kindOfDay: day });
+  } else {
+    day = "Weekday";
+    res.sendFile(__dirname + "/weekday.html");
+  }
 });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
