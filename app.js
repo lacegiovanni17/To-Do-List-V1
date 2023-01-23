7eb62d0eccc418c86faaee2c42f3a21e811d988c
Chidike Henry <dk1online25@gmail.com>
2023-01-23 23:28:07 +0100

    📝added new feat to JS docs

--- app.js
+++ app.js
@@ -4,38 +4,24 @@ const bodyParser = require("body-parser");
 const app = express();
 //the app.use has to be done below the position where you called express in the file.
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Fridday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+
   res.render("list", { kindOfDay: day });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  res.render("List", { newListItem: item});
 });
 
 app.listen(3000, function () {
