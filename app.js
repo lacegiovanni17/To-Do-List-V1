404a6d38ab0f200a5eb179f8c1551ca4b7716411
Chidike Henry <dk1online25@gmail.com>
2023-01-23 21:47:09 +0100

    added new changes to app.js

--- app.js
+++ app.js
@@ -10,12 +10,32 @@ app.get("/", function (req, res) {
   let currentDay = today.getDay();
   var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    day = "Weekend";
-  } else {
-    day = "Weekday";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Fridday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
   }
-  res.render("List", { kindOfDay: day });
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
