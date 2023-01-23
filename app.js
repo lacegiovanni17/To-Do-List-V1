7f31775ce02a2f87a70817b5297ea1e9f7c85922
Chidike Henry <dk1online25@gmail.com>
2023-01-23 23:54:08 +0100

    📝added new feat to JS docs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = [];
 //the app.use has to be done below the position where you called express in the file.
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,12 +17,13 @@ app.get("/", function (req, res) {
   };
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+    res.render("list", { kindOfDay: day, newListItem: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
-  res.render("List", { newListItem: item});
+    item = req.body.newItem;
+    items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
