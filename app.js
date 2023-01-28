88c47ca7e9ddc328f41cf8ee0791ca49a39e0da0
Chidike Henry <dk1online25@gmail.com>
2023-01-28 23:53:25 +0100

    assigning variables properly

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = [];
-//the app.use has to be done below the position where you called express in the file.
+let items = [ "Buy Food", "Cook Food", "Eat Food"];
+//the app.use has to be do"ne below the position where you called express in the file.
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,13 +15,13 @@ app.get("/", function (req, res) {
     day: "numeric",
     month: "long",
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", { kindOfDay: day, newListItem: items });
 });
 
 app.post("/", function (req, res) {
-    item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
   res.redirect("/");
 });
