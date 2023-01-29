907738ad331c7fc1b3702c4ca3301e68e82d435e
Chidike Henry <dk1online25@gmail.com>
2023-01-29 12:47:29 +0100

    added more routes and feat to dir

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
+//I installed the node modules first to activate the node server
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
-//the app.use has to be do"ne below the position where you called express in the file.
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+//the app.use has to be done below the position where you called express in the file.
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//setting the root route
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
+//posting the root route
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -33,15 +29,16 @@ app.post("/", function (req, res) {
   }
 });
 
+//setting the work route
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-// app.post("/work", function (req, res) {
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
+//setting the about route
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
---
+++ date.js
@@ -0,0 +1,21 @@
+//setting up our date.js module
+exports.getDate = function () {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+    return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function () {
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+    return today.toLocaleDateString("en-US", options);
+};
+
